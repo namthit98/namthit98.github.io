a768915888640ac78527e5dedde4126e6b442c9b
NamHandsome <namthit98@gmail.com>
2018-06-12 20:04:22 +0700

    update t project

--- t/js/app.js
+++ t/js/app.js
@@ -129,23 +129,4 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     })
   })
-
-
-
-  ///////////////////////////////////////////
-  /////////////Xử lý side-menu//////////////////
-  ///////////////////////////////////////////
-  document.querySelectorAll(".side-menu__submenu").forEach(function(el) {
-    el.parentElement.addEventListener("click", function() {
-      el.classList.toggle("side-menu__submenu--active");
-
-      let icon = el.previousElementSibling.firstElementChild;
-
-      if(icon.classList.contains("fa-plus-square")) {
-        icon.className = "fas fa-minus-square";
-      } else {
-        icon.className = "fas fa-plus-square";
-      }
-    })
-  })
 })
