dee6655c87e56b986ac2a343a801f7cba7424df8
NamHandsome <namthit98@gmail.com>
2018-06-14 16:38:14 +0700

    finished t project

--- t/js/shared.js
+++ t/js/shared.js
@@ -1,16 +1,1 @@
-    ///////////////////////////////////////////
-    /////////////Xử lý side-menu//////////////////
-    ///////////////////////////////////////////
-    document.querySelectorAll(".side-menu__submenu").forEach(function(el) {
-      el.parentElement.addEventListener("click", function() {
-        el.classList.toggle("side-menu__submenu--active");
-
-        let icon = el.previousElementSibling.firstElementChild;
-
-        if(icon.classList.contains("fa-plus-square")) {
-          icon.className = "fas fa-minus-square";
-        } else {
-          icon.className = "fas fa-plus-square";
-        }
-      })
-    })
+document.querySelectorAll(".side-menu__submenu").forEach(function(a){a.parentElement.addEventListener("click",function(){a.classList.toggle("side-menu__submenu--active");let b=a.previousElementSibling.firstElementChild;b.className=b.classList.contains("fa-plus-square")?"fas fa-minus-square":"fas fa-plus-square"})});
